2580c0ac92fe7f9eb076087b5e453e714e7e40d5
dragonlord1129 <bibhavjha123@gmail.com>
2025-01-25 21:47:24 +0545

    Initial Commit

--- FPGA/RV32I_final/RV32I_final.srcs/sources_1/imports/source/pc_adder.v
+++
@@ -1,9 +0,0 @@
-// ============================
-// PC Adder Module
-// ============================
-module pc_adder (
-    input [31:0] a, b,           // Inputs to the adder
-    output [31:0] c              // Sum output
-);
-    assign c = a + b;            // Perform addition
-endmodule
